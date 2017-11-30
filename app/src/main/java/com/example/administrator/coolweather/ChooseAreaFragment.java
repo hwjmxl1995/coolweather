4131ac57eddc6e46a79b516961fc9de255ca06df
Your Name <you@example.com>
2017-11-30 15:54:24 +0800

    a

--- app/src/main/java/com/example/administrator/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/administrator/coolweather/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.example.administrator.coolweather;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -75,6 +76,13 @@ public class ChooseAreaFragment extends Fragment {
                 } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }
+                else if (currentLevel==LEVEL_COUNTY){
+                    String weatherId=countyList.get(position).getWeatherId();
+                    Intent intent=new Intent(getActivity(),WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
